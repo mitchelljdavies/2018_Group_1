993d869c1a99b0e070a8d7e8358930b1a5242c8e
Mitchell Davies <mitchell@ivatrix.com>
2018-08-04 16:31:44 +1200

    Added LAMP setup, removed DB password, made it so the url is set to the public IP of the server

--- task_1.sh
+++ task_1.sh
@@ -1,15 +1,29 @@
+#!/usr/bin/env bash
+
 # TASK 1 INSTALL FILE
 # This file assumes the following about our instance:
 # - That it's running on Linux 2 AMI (might work on other one's too though, not sure if anything about this is unique to Linux 2 AMI)
-# - That our LAMP Web Server is already set up (So Linux, Apache, MySQL, PHP is installed)
-# - That we've set up the password for the database as "root"
-# - The the Apache web server is running and the database server (MariaDB) is running
-# TODO: You'll note the wp core install command sets the URL as the public IP for the instance I set this up on, you'll have to change that to the public IP of your instance and we'll need to figure out a way we can automatically set this
+
+# LAMP SETUP
+sudo yum update -y && # Install package updates
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 # Install PHP
+sudo yum install -y httpd mariadb-server # Installing Apache & MariaDB
+sudo systemctl start httpd # Start the Apache server
+sudo systemctl enable httpd # Start the Apache server at each system boot
+sudo usermod -a -G apache ec2-user # Adding ourselves as a user to the apache group
+sudo chown -R ec2-user:apache /var/www # Giving the apache group ownership of /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \; # Changing directory permissions
+find /var/www -type f -exec sudo chmod 0664 {} \; # Add group write permissions to /var/www
+sudo systemctl start mariadb # Start the database server
+sudo systemctl enable mariadb # Start the database server at each system boot
+
+# WP SETUP
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar # Download the WP CLI
 chmod +x wp-cli.phar # Make the WP CLI executable
 sudo mv wp-cli.phar /usr/local/bin/wp # Put it in our PATH so we can just call "wp"
 cd /var/www/html # Navigate to our html directory
 wp core download # Download WordPress Core
-wp core config --dbname=wp-db --dbuser=root --dbpass=root # Generate our config file
+wp core config --dbname=wp-db --dbuser=root --dbpass= # Generate our config file
 wp db create # Create the database for our WordPress site
-wp core install --url="http://13.55.180.175" --title="Assignment 1 WordPress Site" --admin_user="group_1" --admin_password="alpha wario salamander" --admin_email="test@test.co.nz" # Install WordPress and set up our user
+IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) # Get the server's public IP
+wp core install --url="http://$IP" --title="Assignment 1 WordPress Site" --admin_user="group_1" --admin_password="alpha wario salamander" --admin_email="test@test.co.nz" # Install WordPress and set up our user
