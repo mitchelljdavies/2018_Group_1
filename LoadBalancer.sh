f3fc94a0c49975b0ee61b0f9f9c8998a983f92a8
phyfferjm <41622239+phyfferjm@users.noreply.github.com>
2018-09-27 20:21:28 +1200

    Load balancer setup script

---
+++ LoadBalancer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# LOAD BALANCER INSTALL FILE
+# This file assumes the following about our instance:
+# - That it's running on Linux 2 AMI (might work on other one's too though, not sure if anything about this is unique to Linux 2 AMI)
+
+# LAMP SETUP
+sudo yum update -y && # Install package updates
+sudo yum install -y httpd haproxy # Installing Apache & HAProxy
