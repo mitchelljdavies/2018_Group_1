f090afaa0306131b8b15468210ad2eb797731fd7
Antony <amsellars@gmail.com>
2018-09-29 09:16:39 -0600

    Began regex for 400 errors, leaving the conditional redirecting to /dev/null

--- checkurl.sh
+++ checkurl.sh
@@ -5,10 +5,10 @@
 # At this stage we only check for 404 or 500 errors, this could be extended to handle other 5xx or 4xx by turning the grep into regex
 # This handles our main purpose which is WordPress being down (which will return a 500 error), other errors like a bad gateway (502) or forbidden (403) doesn't neccessarily denote any action is required
 
-if curl -s --head  --request GET $1 | grep "404" > /dev/null # Check for 404 error on the page
+if curl -s --head  --request GET $1 | grep 4[0-9][0-9] > /dev/null # Check for 404 error on the page
 then
   export problemIP=$1 # Store the Public IP as an environment variable
-  export problemError="404" # Store the error as an environment variable
+  export problemError=$(curl -s --head  --request GET $1 | grep 4[0-9][0-9] | sed 's/[a-z,A-Z]//g') # Store the error as an environment variable
   ansible-playbook email.yml #Send the error notification email
 fi
 if curl -s --head  --request GET $1 | grep "500" > /dev/null # Check for 500 error on the page
