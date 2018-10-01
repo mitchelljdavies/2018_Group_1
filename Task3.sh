ba7a7249ef7c316de6f08a7f19dd77deb210a8c1
Mitchell Davies <mitchell@ivatrix.com>
2018-10-02 08:19:17 +1300

    Pull down CheckInstances.yml if we don't have it on the server

--- Task3.sh
+++ Task3.sh
@@ -4,4 +4,8 @@
 export webmasterEmail=
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_KEY=
+if [ ! -e CheckInstances.yml ] # Check if the CheckInstances.yml exists on the server and if not then pull it down
+then
+  wget https://raw.githubusercontent.com/mitchelljdavies/2018_Group_1/master/CheckInstances.yml #Get CheckInstances.yml
+fi
 ansible-playbook /var/www/html/CheckInstances.yml
