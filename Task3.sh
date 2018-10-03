227889356ebc4c854436b93bf903cc1951730e54
Antony <amsellars@gmail.com>
2018-10-03 13:09:21 -0600

    Added directions to paste environment variable values

--- Task3.sh
+++ Task3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # We export our variables before every time we run the playbook so that the stay persistent
-export webmasterEmail=
-export AWS_ACCESS_KEY_ID=
-export AWS_SECRET_KEY=
+export webmasterEmail= #<-- Add your email here
+export AWS_ACCESS_KEY_ID= #<-- Paste your access key ID here
+export AWS_SECRET_KEY= #<-- Paste your secret key here
 if [ ! -e CheckInstances.yml ] # Check if the CheckInstances.yml exists on the server and if not then pull it down
 then
   wget https://raw.githubusercontent.com/mitchelljdavies/2018_Group_1/master/CheckInstances.yml #Get CheckInstances.yml
