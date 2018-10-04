92f852ef27c4fc023928f439440dd37235336493
phyfferjm <41622239+phyfferjm@users.noreply.github.com>
2018-10-04 14:59:07 +1300

    Update WebServer2.sh

--- WebServer2.sh
+++ WebServer2.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-# WEB SERVER 2 INSTALL FILE
-# This file assumes the following about our instance:
-# - That it's running on Linux 2 AMI (might work on other one's too though, not sure if anything about this is unique to Linux 2 AMI)
-
-# LAMP SETUP
 sudo yum update -y && # Install package updates
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 # Install PHP
 sudo yum install -y httpd mariadb-server # Installing Apache & MariaDB
@@ -17,13 +11,11 @@ find /var/www -type f -exec sudo chmod 0664 {} \; # Add group write permissions 
 sudo systemctl start mariadb # Start the database server
 printf '\ny\ntest\ntest\ny\ny\ny\ny\n' | sudo mysql_secure_installation # Secure SQL installation
 sudo systemctl enable mariadb # Start the database server at each system boot
-
-# WP SETUP
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar # Download the WP CLI
 chmod +x wp-cli.phar # Make the WP CLI executable
 sudo mv wp-cli.phar /usr/local/bin/wp # Put it in our PATH so we can just call "wp"
 cd /var/www/html # Navigate to our html directory
-wp core download # Download WordPress Core
+wp core download --version=3.9 # Download WordPress Core
 wp core config --dbname=wpdb --dbuser=root --dbpass=test # Generate our config file
 wp db create # Create the database for our WordPress site
 IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) # Get the server's public IP
