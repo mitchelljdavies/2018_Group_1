0686659f3b17d47f58b9e01fcf59f646917e1501
phyfferjm <41622239+phyfferjm@users.noreply.github.com>
2018-10-04 16:41:21 +1300

    Update ProxyConfig.sh

--- ProxyConfig.sh
+++ ProxyConfig.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+cd /etc/haproxy/
 sudo systemctl stop haproxy
 sudo sed -i 's/frontend  main *:5000/frontend  main *:80/g' /etc/haproxy/haproxy.cfg
 sudo sed -i '64,67d' /etc/haproxy/haproxy.cfg
-sudo sed -i 's/127.0.0.1:5001/'$IP1:80/g /etc/haproxy/haproxy.cfg
-sudo sed -i 's/127.0.0.1:5002/'$IP2:80/g /etc/haproxy/haproxy.cfg
-sudo sed -i 's/127.0.0.1:5003/'$IP3:80/g /etc/haproxy/haproxy.cfg
+sudo sed -i 's/127.0.0.1:5001/'$IP1:80/g haproxy.cfg
+sudo sed -i 's/127.0.0.1:5002/'$IP2:80/g haproxy.cfg
+sudo sed -i 's/127.0.0.1:5003/'$IP3:80/g haproxy.cfg
 sudo systemctl start haproxy
