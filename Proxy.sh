21932d4349707a44fd53371b0f615635c5037e70
phyfferjm <41622239+phyfferjm@users.noreply.github.com>
2018-10-04 16:24:39 +1300

    Update and rename LoadBalancer.sh to Proxy.sh

--- Proxy.sh
+++ Proxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# LOAD BALANCER INSTALL FILE
+# PROXY INSTALL FILE
 # This file assumes the following about our instance:
 # - That it's running on Linux 2 AMI (might work on other one's too though, not sure if anything about this is unique to Linux 2 AMI)
 
